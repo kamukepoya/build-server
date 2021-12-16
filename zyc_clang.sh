8daef5fc3e9f2a06eec3a0444a883d7f136767d7
kamukepo <91080807+kamukepoya@users.noreply.github.com>
2021-12-16 11:28:31 +0700

    Update zyc_clang.sh

--- zyc_clang.sh
+++ zyc_clang.sh
@@ -53,7 +53,8 @@ cd mt6768
 make -j$(nproc) O=out ARCH=arm64 merlin_defconfig
 make -j$(nproc) ARCH=arm64 O=out \
     CC=${CLANG_ROOTDIR}/bin/clang \
-    AR=${CLANG_ROOTDIR}/bin/llvm.as \
+    NM=${CLANG_ROOTDIR}/bin/llvm-nm \
+    LD=${CLANG_ROOTDIR}/bin/ld.lld \
     CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
 
