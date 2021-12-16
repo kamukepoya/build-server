e38fe1193be5d64bebe7b68b99f160c820f7fc6d
kamukepo <91080807+kamukepoya@users.noreply.github.com>
2021-12-16 08:15:21 +0700

    Update zyc_clang.sh

--- zyc_clang.sh
+++ zyc_clang.sh
@@ -34,7 +34,7 @@ DTBO=mt6768/out/arch/arm64/boot/dtbo.img
 DTB=mt6768/out/arch/arm64/boot/dts/mediatek/dtb
 DATE=$(date +"%F"-"%S")
 START=$(date +"%s")
-PATH=${CLANG_ROOTDIR}/bin:${PATH}
+PATH=${PATH}:${CLANG_ROOTDIR}
 
 # Tg export
 export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
@@ -50,17 +50,12 @@ tg_post_msg() {
 tg_post_msg "<b>Compiled has started</b>"
 function compile(){
 cd mt6768
-make -j$(nproc) O=out ARCH=arm64 merlin_defconfig
+make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
 make -j$(nproc) ARCH=arm64 O=out \
     CC=${CLANG_ROOTDIR}/bin/clang \
-    AR=${CLANG_ROOTDIR}/bin/llvm-ar \
-    NM=${CLANG_ROOTDIR}/bin/llvm-nm \
-    OBJCOPY=${CLANG_ROOTDIR}/bin/llvm-objcopy \
-    OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
-    STRIP=${CLANG_ROOTDIR}/bin/llvm-strip \
-    LD=${CLANG_ROOTDIR}/bin/ld.lld \
     CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
+
    if ! [ -a "$IMAGE" ]; then
 	finerr
        exit 1
